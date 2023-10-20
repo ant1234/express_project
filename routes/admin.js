f06a22b45929fab8a32ad295958d664b38bbd936
unknown <antonyvidetta1986@gmail.com>
2023-10-20 16:21:41 +1300

    Add ejs template files

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {PageTite: 'Add Product' });
+  res.render('add-product', {pageTitle: 'Add Product', path: 'admin/add-product', formCSS: true, productCSS: true, activeAddProduct: true});
 });
 
 // /admin/add-product => POST

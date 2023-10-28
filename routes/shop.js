4736febf8ad3d04b06beb0d32e044b8f7f7feba9
ant1234 <antonyvidetta@gmail.com>
2023-10-28 21:31:56 +1300

    Add new layout

--- routes/shop.js
+++ routes/shop.js
@@ -10,12 +10,12 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   res.render('shop', {
-    prods: products, 
+    prods: products,
     pageTitle: 'Shop',
     path: '/',
     hasProducts: products.length > 0,
-    activeShop: true,  
-    productCSS: true,
+    activeShop: true,
+    productCSS: true
   });
 });
 
